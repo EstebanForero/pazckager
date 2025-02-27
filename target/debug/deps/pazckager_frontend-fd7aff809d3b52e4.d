5db2f3684151937d3f8c521d233b6f81da5a1a96
Esteban Forero <estebanmff@outlook.com>
2025-02-26 20:40:31 -0500

    Added main crates

---
+++ target/debug/deps/pazckager_frontend-fd7aff809d3b52e4.d
@@ -0,0 +1,5 @@
+/home/esteban/Documents/projects/pazckager/target/debug/deps/pazckager_frontend-fd7aff809d3b52e4: pazckager_frontend/src/main.rs
+
+/home/esteban/Documents/projects/pazckager/target/debug/deps/pazckager_frontend-fd7aff809d3b52e4.d: pazckager_frontend/src/main.rs
+
+pazckager_frontend/src/main.rs:
---
+++ target/debug/pazckager_frontend.d
@@ -0,0 +1,1 @@
+/home/esteban/Documents/projects/pazckager/target/debug/pazckager_frontend: /home/esteban/Documents/projects/pazckager/pazckager_frontend/src/main.rs
